3328b102c1636a18e592cf1411d9e90cbc5e158a
Kim-Young-Gon <kimyg3174@gmail.com>
2018-11-20 13:35:22 +0900

    확인용 로그 추가

--- src/com/test/SampleSuccessTest.java
+++ src/com/test/SampleSuccessTest.java
@@ -1,5 +1,8 @@
 package com.test;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 public class SampleSuccessTest {
     public static void main(String args[]) throws Exception {
         System.out.println("SampleSuccessTest ====> start");
@@ -8,5 +11,10 @@ public class SampleSuccessTest {
             Thread.sleep(1000);
         }
         System.out.println("SampleSuccessTest ====> end");
+        SimpleDateFormat date = new SimpleDateFormat();
+        date.applyPattern("yyyy-MM-dd HH:mm:ss");
+        String now = date.format(Calendar.getInstance().getTime());
+        int success = (int) Math.round(Math.random() * 1000);
+        System.out.println(now + " TEST " + success);
     }
 }
